4dab9acec29705ee49c7a8e45760eafb5610c1c7
Petar Nikov <petar.nikov@gmail.com>
2021-06-21 06:43:20 +0300

    Ambassador: add gorm and database init.

--- go-ambassador/go.mod
+++ go-ambassador/go.mod
@@ -2,4 +2,8 @@ module ambassador
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.13.0 // indirect
+require (
+	github.com/gofiber/fiber/v2 v2.13.0 // indirect
+	gorm.io/driver/mysql v1.1.1 // indirect
+	gorm.io/gorm v1.21.11 // indirect
+)
--- go-ambassador/main.go
+++ go-ambassador/main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	dsn := "root:root@tcp(db:3306)/ambassador?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect with the database!")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":8000")
 }
